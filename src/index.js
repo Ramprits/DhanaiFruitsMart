d7b14116f97accd7aa52870da9a0f29b6a480bfa
ramprits <rampritsahani@outlook.in>
2019-02-24 12:42:45 +0530

    my first commit after delete all generate file and code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React and ReactDOM library
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create react component
+const App = () => {
+  return (
+    <Fragment>
+      <h2 style={{ color: "red" }}>Hi there! </h2>
+      <p>Welcome to React world!</p>
+    </Fragment>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default App;
+
+// take the react component and show on the screen.
+
+ReactDOM.render(<App />, document.querySelector("#root"));
